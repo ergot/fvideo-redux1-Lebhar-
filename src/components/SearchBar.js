b8d58e08b686aeb8a88b41cc76032ab99263c9ee
ergot <wjose14@hotmail.com>
2017-10-19 14:19:57 +0200

    2-8

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 class  SearchBar extends React.Component {
-    render(){
-        return <input/>
+    state = {
+        searchText: '',
+        placeHolder: 'Tapez votre film'
+    }
+
+    handleChange = (event) => {
+        this.setState({searchText: event.target.value})
+    }
+
+    render() {
+        return (
+        <div>
+            <input onChange={this.handleChange} placeholder={this.state.placeHolder}/>
+            <p>{this.state.searchText}</p>
+        </div>
+        )
     }
 }
 

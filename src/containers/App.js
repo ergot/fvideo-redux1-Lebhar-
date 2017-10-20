f731fd77126a11c5ec5b95deeb60a60ab93b8e15
ergot <wjose14@hotmail.com>
2017-10-20 14:59:36 +0200

    2-16

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import SearchBar from '../components/SearchBar'
 import VideoList from './VideoList'
+import axios from 'axios'
 
 
-const App = () => {
-    return <div>
-        <SearchBar/>
-        <VideoList/>
-    </div>
+const API_END_POINT = 'https://api.themoviedb.org/3/'
+const POPULAR_MOVIES_URL = 'discover/movie?'
+const API_KEY = 'api_key=d0202279211bbec3135bba06af0b8933'
+
+class App extends React.Component {
+
+    state = {movies:{}, currentMovie:{}}
+
+    componentWillMount = () => {
+        axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(
+            (response)=>{
+                this.setState({movies: response.data.results.slice(1,6), currentMovie: response.data.results[0]})
+            }
+        )
+    }
+
+
+    render(){
+        return <div>
+            <SearchBar/>
+            <VideoList/>
+        </div>
+    }
+
 }
 
 export default App

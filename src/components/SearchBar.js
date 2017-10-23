f7915a82c50a56a16a7565f17b601adea9d6719d
ergot <wjose14@hotmail.com>
2017-10-23 12:24:57 +0200

    2-25

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,29 @@ import React from 'react'
 class  SearchBar extends React.Component {
     state = {
         searchText: '',
-        placeHolder: 'Tapez votre film'
+        placeHolder: 'Tapez votre film',
+        intervalBeforeRequest:1000,
+        lockRequest: false
     }
 
     handleChange = (event) => {
         this.setState({searchText: event.target.value})
+        if(!this.state.lockRequest) {
+            console.log('yooooooooolo')
+            this.setState({lockRequest:true})
+            setTimeout(()=>{
+                this.search()
+            }, this.state.intervalBeforeRequest)
+        }
     }
 
-    handleOnClick =  (event) => {
+    handleOnClick =  () => {
+        this.search()
+    }
+
+    search = () => {
         this.props.callback(this.state.searchText)
+        this.setState({lockRequest:false})
     }
 
     render() {
